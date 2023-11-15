8be7c7a9c3972186577e829cbb045227b9651092
Furno <furnoburno.fb@gmail.com>
2023-11-15 09:36:07 +0100

    gotowy program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // TODO wpisz 1 string, przenies 2 ostatnie znaki na przod
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Wpisz tekst: ");
+        String string = scanner.nextLine();
+
+        if (string.length() > 2) {
+            String stringTwoLastLetters = string.substring(string.length() - 2);
+            System.out.println(stringTwoLastLetters + string.substring(0, string.length() - 2));
+        }
+        else {
+            System.out.println("String powinien mieć więcej niż 2 znaki.");
+        }
     }
 }
